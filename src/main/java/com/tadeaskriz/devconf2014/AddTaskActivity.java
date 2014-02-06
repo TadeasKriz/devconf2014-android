99cdcd93defdf55083c73874327fd49753680033
Tadeas Kriz <tadeas.kriz@gmail.com>
2014-02-06 20:17:36 +0100

    Add task adding.

--- src/main/java/com/tadeaskriz/devconf2014/AddTaskActivity.java
+++ src/main/java/com/tadeaskriz/devconf2014/AddTaskActivity.java
@@ -2,7 +2,13 @@ package com.tadeaskriz.devconf2014;
 
 import android.app.Activity;
 
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * @author <a href="mailto:tadeas.kriz@brainwashstudio.com">Tadeas Kriz</a>
@@ -11,5 +17,18 @@ import org.androidannotations.annotations.EActivity;
 public class AddTaskActivity extends Activity {
 
 
+    @ViewById
+    EditText text;
+
+    @ViewById
+    Button add;
+
+    @Click(R.id.add)
+    void addItem() {
+        Intent data = new Intent();
+        data.putExtra("text", text.getText().toString());
+        setResult(RESULT_OK, data);
+        finish();
+    }
 
 }
--- src/main/java/com/tadeaskriz/devconf2014/MainActivity.java
+++ src/main/java/com/tadeaskriz/devconf2014/MainActivity.java
@@ -2,39 +2,39 @@ package com.tadeaskriz.devconf2014;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.App;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OnActivityResult;
+import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 import org.jboss.aerogear.android.Callback;
 import org.jboss.aerogear.android.Pipeline;
 import org.jboss.aerogear.android.impl.pipeline.PipeConfig;
-import org.jboss.aerogear.android.pipeline.AbstractActivityCallback;
 import org.jboss.aerogear.android.pipeline.Pipe;
 import org.jboss.aerogear.android.unifiedpush.MessageHandler;
 import org.jboss.aerogear.android.unifiedpush.Registrations;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 @EActivity(R.layout.activity_main)
 @OptionsMenu(R.menu.main)
 public class MainActivity extends Activity implements MessageHandler {
+
+    private static final int REQUEST_ADD = 1;
 
     @ViewById
     LinearLayout unfinishedTasks;
@@ -74,11 +74,9 @@ public class MainActivity extends Activity implements MessageHandler {
         config.setEndpoint("tasks");
 
         tasksPipe = mainPipeline.pipe(Task.class, config);
-        
+
         reloadTasks();
     }
-
-
 
     private void reloadTasks() {
         tasksPipe.read(new Callback<List<Task>>() {
@@ -101,16 +99,16 @@ public class MainActivity extends Activity implements MessageHandler {
 
         LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
 
-        for(final Task task : tasks) {
+        for (final Task task : tasks) {
             LinearLayout targetLayout;
 
-            if(task.isDone()) {
+            if (task.isDone()) {
                 targetLayout = finishedTasks;
             } else {
                 targetLayout = unfinishedTasks;
             }
             final View taskItem = inflater.inflate(R.layout.task_item, targetLayout, false);
-            if(taskItem == null) {
+            if (taskItem == null) {
                 continue;
             }
             CheckBox done = (CheckBox) taskItem.findViewById(R.id.done);
@@ -136,6 +134,31 @@ public class MainActivity extends Activity implements MessageHandler {
             text.setText(task.getText());
 
             targetLayout.addView(taskItem);
+        }
+    }
+
+    @OptionsItem(R.id.action_add)
+    void addNewTask() {
+        AddTaskActivity_.intent(this).startForResult(REQUEST_ADD);
+    }
+
+    @OnActivityResult(REQUEST_ADD)
+    void onAddNewTaskResult(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            String text = data.getStringExtra("text");
+
+            Task task = new Task();
+            task.setText(text);
+
+            tasksPipe.save(task, new Callback<Task>() {
+                @Override
+                public void onSuccess(Task task) {
+                }
+
+                @Override
+                public void onFailure(Exception e) {
+                }
+            });
         }
     }
 
